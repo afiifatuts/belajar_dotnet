f90f8ea75a1c5e68416fb9d338509ce07b7d3982
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-06-19 17:21:53 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       int number =10;//eksplisit
+       var number2 =10;//inplisit
+       
+       /*
+       Number data type 
+       tipe data number dibagi menjadi 2: 
+       *Integer Types :
+       -byte 8bit 
+       -short 16bit 
+       -long 64bit
+       *floating point type:
+       -float32 bit 
+       -double 64bit decimal 128bit 
+       */
+       
+       //integer types
+       byte byteNumber 128;
+       short shortNumber = 10000;
+       int intNumber=1800000;
+       long longNumber =100000000l;
+       
+       //floating point types :
+       float floatNumber = 12345.6f;
+       double doubleNumber = 123456789.6d;
+    }
+}
