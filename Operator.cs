d3153b3960296b90318a8839b8dff8bfc06158d4
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-06-19 19:27:30 +0700

    operator

---
+++ Operator.cs
@@ -0,0 +1,47 @@
+namespace FundamentalUpskilling;
+
+public class Operator
+{
+    public static void Main(string[] arg)
+    {
+        /*
+        Aritmatic Operator 
+        (+) (-) (*) (/) (%)
+        */
+        //tipe data yang aman untuk int dan double
+        int a = 10 +20;
+        Console.WriteLine(a);
+        double b = 10.4 +20;
+        Console.WriteLine(b);
+
+        /*
+        Assignment Operator
+        (=)
+        */
+
+        int c =10;
+        Console.WriteLine(c);
+
+         /*
+        Assignment Comprasion
+        <,>,<=,>=, == , !=
+        */
+
+        bool d = 10>0;
+        Console.WriteLine(d);
+
+        //increment, decrement (x++, x--, ++x, --x)
+
+        Console.WriteLine(++c);
+
+         /*
+        Logical Operator
+        && ||
+        */
+
+        bool e = 10 > 0 && 0 <10;
+        Console.WriteLine(e);
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,31 +2,58 @@
 
 public class Program
 {
-    public static void Main(string[] args)
+    public  void Main()
     {
-       int number =10;//eksplisit
-       var number2 =10;//inplisit
-       
-       /*
-       Number data type 
-       tipe data number dibagi menjadi 2: 
-       *Integer Types :
-       -byte 8bit 
-       -short 16bit 
-       -long 64bit
-       *floating point type:
-       -float32 bit 
-       -double 64bit decimal 128bit 
-       */
-       
-       //integer types
-       byte byteNumber 128;
-       short shortNumber = 10000;
-       int intNumber=1800000;
-       long longNumber =100000000l;
-       
-       //floating point types :
-       float floatNumber = 12345.6f;
-       double doubleNumber = 123456789.6d;
-    }
+//        int number =10;//eksplisit
+//        var number2 =10;//inplisit
+//        
+//        /*
+//        Number data type 
+//        tipe data number dibagi menjadi 2: 
+//        *Integer Types :
+//        -byte 8bit 
+//        -short 16bit 
+//        -long 64bit
+//        *floating point type:
+//        -float32 bit 
+//        -double 64bit decimal 128bit 
+//        */
+//        
+//        //integer types
+//        byte byteNumber 128;
+//        short shortNumber = 10000;
+//        int intNumber=1800000;
+//        long longNumber =100000000l;
+//        
+//        //floating point types :
+//        float floatNumber = 12345.6f;
+//        double doubleNumber = 123456789.6d;
+
+//     /*
+//     String data types
+//     */
+    
+//     char myChar = 'a';
+//     string fullName = 'afiifatuts tsaaniyah abdullah';
+
+   // //Escape character
+   // string text = "This is \" string \" ";
+   // //Verbatim strings
+    // string path = @"\\mypc\shared\project";
+    // Console.WriteLine(path);
+    // Console.WriteLine(@"1. Belajar
+    // 2. Makan
+    // 3.Tidur");
+    
+    //Concatenation
+    string nama = "afiifatuts" +" tsaaniyah";
+
+    //string interpolation
+    string firstName = "afiifatuts";
+    string lastName ="tsaaniyah";
+    string fullName2 = $"{firstName} {lastName}";
+    Console.WriteLine(nama);
+    Console.WriteLine(fullName2);
+
+}
 }
